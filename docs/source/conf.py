603d8ea519dd01b0ff08e58b3365c08df929d45c
Moritz Lange <moritz.lange@uni-ulm.de>
2018-04-18 11:34:54 +0200

    Corrected mistake in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 MOCK_MODULES = ['mpnum', 'py-orthpol', 'orthpol']
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+	sys.modules[mod_name] = mock.Mock()
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
