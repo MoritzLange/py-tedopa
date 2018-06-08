badf0adc81a96d2cf95acb4d0146ffac8fbab9a3
Moritz Lange <moritz.lange@uni-ulm.de>
2018-06-08 15:59:01 +0200

    Added test for function tedopa_models.calculate_expectation_values()

--- tests/test_tedopa_models.py
+++ tests/test_tedopa_models.py
@@ -4,6 +4,8 @@ Tests for the functions in tedopa/tedopa_models.py
 
 import pytest as pt
 import numpy as np
+from numpy.testing import assert_almost_equal
+import mpnum as mp
 
 from tedopa import tedopa_models as tm
 
@@ -17,3 +19,13 @@ def test_create_bosonic_vacuum_state(system_site_states, len_chain,
     state = tm.create_bosonic_vacuum_state(system_site_states, len_chain,
                                            dim_oscillators)
     assert len(state) == len(system_site_states) + sum(len_chain)
+
+
+@pt.mark.parametrize('sites, dim', [(1, 3), (2, 2)])
+def test_calculate_expectation_values(sites, dim):
+    mpo = mp.random_mpa(sites=sites, ldim=(dim, dim), rank=1)
+    obsvbl = np.random.random([dim ** sites] * 2)
+    expct_value1 = np.trace(np.dot(mpo.to_array_global().reshape([dim ** sites]
+                                                                 * 2), obsvbl))
+    expct_value2 = tm.calculate_expectation_values([mpo], obsvbl)[0]
+    assert_almost_equal(expct_value1, expct_value2)
